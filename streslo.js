2dcb69ca807a1c41132049627dee571d964bf3b4
Aldi Perdana <me@aldidana.com>
2014-11-06 05:52:24 +0700

    masuk

---
+++ streslo.js
@@ -0,0 +1,50 @@
+Boards = new Mongo.Collection('boards')
+
+if(Meteor.isClient) {
+
+	Template.boards.helpers({
+		boards: function() {
+			return Boards.find({}, {sort: {order: 1}})
+		}
+	})
+
+	Template.boards.rendered = function() {
+		this.$('#boards').sortable({
+			stop: function(e, ui) {
+				el = ui.item.get(0)
+				before = ui.item.prev().get(0)
+				after = ui.item.next().get(0)
+
+
+				if(!before) {
+					newOrder = Blaze.getData(after).order - 1
+				} else if(!after) {
+					newOrder = Blaze.getData(before).order + 1
+				}
+				else
+					newOrder = (Blaze.getData(after).order + Blaze.getData(before).order)/2
+
+				Boards.update({_id: Blaze.getData(el)._id}, {$set: {order: newOrder}})
+			}
+		})
+	}
+
+	Template.boards.events({
+		"click .delete": function() {
+			Boards.remove(this._id);
+		}
+	})
+} 
+
+if(Meteor.isServer) {
+	Meteor.startup(function() {
+		if(Boards.find({}).count() == 0) {
+			for (var i = 1; i<=5; i++) {
+				Boards.insert({
+					judul: "judul " + i,
+					order: i
+				});
+			}
+		}
+	});
+}
